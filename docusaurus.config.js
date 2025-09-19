4ab0e492e73cbf1fe406e0439a81b687af09828f
Scot-Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2025-09-19 12:15:52 +0100

    fix(config): missing double quote

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 const organizationName = "Scot-Survivor";
-const projectName = "Shimincraft-Wiki;
+const projectName = "Shimincraft-Wiki";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
