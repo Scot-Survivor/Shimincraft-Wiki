e154cd9022e76d0e04e3227fd7cedc1faea4458c
Scot-Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2025-09-19 20:19:08 +0100

    feat: disable footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,38 +70,7 @@ const config = {
               { to: "/blog", label: "Dev Blog", position: "left" },
           ],
       },
-      footer: {
-          style: "dark",
-          links: [
-              {
-                  title: "Docs",
-                  items: [
-                      {
-                          label: "Glossary",
-                          to: "/glossary",
-                      },
-                      {
-                          label: "Documentation",
-                          to: "/docs/intro",
-                      },
-                  ],
-              },
-              {
-                  title: "More",
-                  items: [
-                      {
-                          label: "Blog",
-                          to: "/blog",
-                      },
-                      {
-                          label: "GitHub of Wiki",
-                          href: `https://github.com/${organizationName}/${projectName}`,
-                      },
-                  ],
-              },
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
+      footer: {},
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     colorMode: {
         defaultMode: "dark",
